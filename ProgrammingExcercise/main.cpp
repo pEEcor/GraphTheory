c3776b7c617cf0d350738037e7cec21584d24145
pEEcor <pEEcor@xcor.org>
2016-04-13 19:05:24 +0200

    initial commit
    - add .gitignore
    - xcode project
    - main.cpp containing hello world
    
    Signed-off-by: pEEcor <pEEcor@xcor.org>

---
+++ ProgrammingExcercise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProgrammingExcercise
+//
+//  Created by Paavo Becker on 13.04.16.
+//  Copyright © 2016 Paavo Becker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
